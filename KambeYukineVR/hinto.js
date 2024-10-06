aabb22e327c56d5387c94746655930803c2e268d
sekiyuki-2013 <147236095+sekiyuki-2013@users.noreply.github.com>
2024-10-06 18:36:33 +0900

    Update hinto.js
    ヒントクラスの実例をわかりやすくしました

--- KambeYukineVR/hinto.js
+++ KambeYukineVR/hinto.js
@@ -1,7 +1,20 @@
+//ヒントを管理するファイル
 
-hinto_kanri.push(new Hinto(1800 - 100, "残り1分"));
+//ヒントの書き方
+//hinto_kanri.push(new Hinto(1800 - 表示したい秒数*10, "表示したい文字"));
+hinto_kanri.push(new Hinto(1800 - 100, "10秒経過"));
 
-hinto_kanri.push(new Hinto(1800 - 50, "残り2分"));
+hinto_kanri.push(new Hinto(1800 - 50, "5秒経過"));
+
+hinto_kanri.push(new Hinto(1500, "残り2分半"));
 
 
+
+
+//ここら辺に追加してね
+
+
+
+
+//これより下に書かないでください
 hinto_kanri.sort();
